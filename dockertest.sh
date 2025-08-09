44560685ca7ad90029dcd99e4ca3ff80020b228e
mrchcat <m-cat@mail.ru>
2025-08-09 18:24:48 +0300

    initial commit

--- dockertest.sh
+++ dockertest.sh
@@ -1,7 +1,7 @@
-sh 'echo 'dckr_pat_KfJQlpmEAHL_9ccoc5ghNPrvzew' | sudo docker login -u 'dckr_pat_KfJQlpmEAHL_9ccoc5ghNPrvzew' --password-stdin'
-
- A Personal Access Token (PAT) can be used instead.
-docker login --username mcat1980
-
-echo 'dckr_pat_KfJQlpmEAHL_9ccoc5ghNPrvzew' | docker login --username mcat1980 --password-stdin
+#sh 'echo 'dckr_pat_KfJQlpmEAHL_9ccoc5ghNPrvzew' | sudo docker login -u 'dckr_pat_KfJQlpmEAHL_9ccoc5ghNPrvzew' --password-stdin'
+#
+# A Personal Access Token (PAT) can be used instead.
+#docker login --username mcat1980
+#
+#echo 'dckr_pat_KfJQlpmEAHL_9ccoc5ghNPrvzew' | docker login --username mcat1980 --password-stdin
 
